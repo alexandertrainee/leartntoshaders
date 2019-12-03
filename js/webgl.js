d18d5bb915cb7230277df1e84a5f2f7809f3d3e5
layuk.alexander <alexander.layuk@banuba.com>
2019-12-04 00:14:09 +0300

    создал две функции создание шейдеров и программы, отрефакторил по замечаниям

--- js/webgl.js
+++ js/webgl.js
@@ -1,22 +1,23 @@
-var initWebgl = function(){
-    var VS;
-    var FS;
-    loadTextResource("../shaders/vertexShader.glsl").then(function(result){
+function initWebgl(){
+    let VS;
+    let FS;
+    loadTextResource("../shaders/vertexShader.glsl").then(result => {
+        console.log(result);
         VS = result;
         return loadTextResource("../shaders/fragmentShader.glsl");
-    }).then(function(result){
+    }).then(result => {
+        console.log(result);
         FS = result;
         return StartWebgl(VS,FS);
-    }).catch(function(error){
-        alert("some error happened");
-        console.error(error);
+    }).catch(error =>{
+        return new Error("some error happened" + error);
     })
 }
-var loadTextResource = function(url){
-    return new Promise(function(resolve,reject){
+function loadTextResource(url){
+    return new Promise((resolve,reject) => {
         var request = new XMLHttpRequest();
         request.open("GET" ,url, true);
-        request.onload = function(){
+        request.onload = () => {
             if (request.status >= 200 && request.status < 300){
                 resolve(request.responseText);
             } else {
@@ -27,9 +28,64 @@ var loadTextResource = function(url){
     })
 }
 
+function createShader(gl, type, source){
+    let shader = gl.createShader(type);
 
-var StartWebgl = function(vertexShader, fragmentShader){
-    alert("all is okey")
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+    if (success){
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return undefined;
+}
+
+function createProgram(gl, vertexShader, fragmentShader){
+    let program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    gl.validateProgram(program);
+
+    let success = gl.getProgramParameter(program, gl.VALIDATE_STATUS);
+
+    if(success){
+        return program;
+    }
+
+    console.log(gl.gsetProgramInfoLog(program));
+    gl.deleteProgram(program);
+    return undefined;
+}
+
+function StartWebgl(vertexSource, fragmentSource){
+
+    let canvas = document.getElementById("canvas");
+    let gl = canvas.getContext("webgl2");
+
+    if(!gl){
+        return new Error("webgl2 not loaded :c");
+    } else {
+        console.log("all is okey");
+    }
+
+    canvas.height = gl.canvas.clientHeight;
+    canvas.width = gl.canvas.clientWidth;
+
+    gl.viewport(0,0, gl.canvas.height, gl.canvas.width);
+
+   console.log(gl);
+
+    let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
+    let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+
+    let program = createProgram(gl, vertexShader, fragmentShader);
+
 }
 
 document.addEventListener("DOMContentLoaded" , function(){
